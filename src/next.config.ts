9e3bb7e0b502e0c235350d14dece0c1651966e90
Gourav pathak <gourav.visual@gmail.com>
2025-06-29 11:58:15 +0000

    I'm trying to host thhis application in IIS however, assets are nnot loa

---
+++ src/next.config.ts
@@ -0,0 +1,24 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  basePath: '/deploytrack',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
