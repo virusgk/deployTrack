ab3fe1f1a64559091383488666bcfa756989b854
Gourav pathak <gourav.visual@gmail.com>
2025-06-29 12:24:52 +0000

    In IIS I've. reated the app name deploytrack within https app. And I've

---
+++ server.js
@@ -0,0 +1,22 @@
+
+// server.js
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: __dirname });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
